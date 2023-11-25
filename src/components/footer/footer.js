84da958da461436a0e4b310b2ae850e103660235
Jaime <quezadajaime@ymail.com>
2023-11-25 09:05:30 -0800

    Fixing footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,7 +5,7 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import EmailIcon from "@mui/icons-material/Email";
-import "./Footer.css";
+import "./footer.css";
 
 // Function that renders the footer
 const Footer = () => {
